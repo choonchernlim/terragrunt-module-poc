79441abd808438800b67c5073928734b19bf3795
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2022-11-21 12:04:56 -0600

    poc

---
+++ folder/variables.tf
@@ -0,0 +1,9 @@
+variable "parent_folder_id" {
+  description = "ID of parent folder"
+  type        = string
+}
+
+variable "folder_name" {
+  description = "Name of the folder"
+  type        = string
+}
